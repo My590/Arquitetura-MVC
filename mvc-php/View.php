9372237d849eea922dcc301647ea63ab8bac09a3
Milena de Bastos <milenabmiranda1234@gmail.com>
2026-06-10 18:08:37 +0000

    conclusão dos arquivos

--- mvc-php/View.php
+++ mvc-php/View.php
@@ -6,15 +6,20 @@ class View {
 
     public function __construct(Model $model) {
         $this->model = $model;
-    }
+    
 }
 
 public function output() {
-    return "
-    <h1> Calculadora MVC </h1> 
-    <p> Número 1: {$this->model->numero1} </p>
-    
-    
-    
-    "
+        return "
+        <h1> Calculadora MVC </h1> 
+        <p> Número 1: {$this->model->numero1} </p>
+        <p> Número 2: {$this->model->numero2} </p>
+        <p> <strong>Resultado: </strong> {$this->model->resultado}</p>
+        <a href= 'index.php?action=somar'>Somar </a>|
+        <a href= 'index.php?action=subtrair'>Subtrair </a>|
+        <a href= 'index.php?action=multiplicar'>Multiplicar </a>|
+        <a href= 'index.php?action=dividir'>Dividir </a>|
+        ";
+
+    }
 }
